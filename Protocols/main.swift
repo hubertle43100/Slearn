981b63c36495519cd20809d4c680fbc51af1d9ab
Hubert <hubertle43100@gmail.com>
2020-10-31 16:08:22 -0700

    learned about protocols and how they are used like certificates and can be inherit by class/struct

---
+++ Protocols/main.swift
@@ -0,0 +1,69 @@
+//certification on flying
+protocol CanFly {
+    func fly() // can't have '{ }' in protocol
+}
+
+
+class Bird {
+    
+    var isFemale = true
+    
+    func layEgg(){
+        if isFemale {
+            print("The bird makes a new bird in a shell.")
+        }
+    }
+    
+//    func fly() {
+//        print("The bird flaps its wings and lifts off into the sky.")
+//    }
+}
+
+class Eagle: Bird, CanFly {
+    func fly() {
+        print("The bird flaps its wings and lifts off into the sky.")
+    }
+    
+    
+    func soar(){
+        print("The eagle glides in the air using air currents.")
+    }
+}
+
+class Penguin: Bird {
+    func swim() {
+        print("The penguin paddles through the water.")
+    }
+}
+
+struct FlyingMuseum {
+    func flyingDemo(flyingObject: CanFly) { //bird --> Canfly
+        flyingObject.fly()
+    }
+}
+
+//struct Airplane: {  has to be a class in order to inherit
+    
+//Proctcol can be inherit by struct and classes
+struct Airplane: CanFly{ //changed back to a struct
+    func fly() { //OVERRIDE: force override (headache are given b/c it inherits bird
+        print("The airplane uses its engine to lift off into the air.")
+    }
+}
+
+let myEagle = Eagle()
+myEagle.fly()
+myEagle.layEgg()
+myEagle.soar()
+
+let myPenguin = Penguin()
+myPenguin.layEgg()
+myPenguin.swim()
+//myPenguin.fly() //penguin can't fly
+
+let myPlane = Airplane() //not part of bird family
+//myPlane.layEgg() //stupid stupid stupid
+myPlane.fly()
+
+let museum = FlyingMuseum()
+museum.flyingDemo(flyingObject: myEagle) //can't pass in penguin cause it cannot fly
